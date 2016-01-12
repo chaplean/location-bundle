69c28675383b85d7f2ef2229272b850c433a513e
Valentin Bontemps <valentin@chaplean.com>
2016-01-12 06:39:38 +0100

    add new region --skip-ci

---
+++ Controller/Rest/LocationController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Chaplean\Bundle\LocationBundle\Controller\Rest;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Criteria;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations;
+use JMS\Serializer\SerializationContext;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * LocationController.php.
+ *
+ * @author    Valentin - Chaplean <valentin@chaplean.com>
+ * @copyright 2014 - 2015 Chaplean (http://www.chaplean.com)
+ * @since     3.0.0
+ *
+ * @Annotations\RouteResource("Location")
+ */
+class LocationController extends FOSRestController
+{
+    /**
+     * @Annotations\Get("/location/search/{location}")
+     *
+     * @param string $location
+     *
+     * @return Response
+     */
+    public function getLocationAction($location)
+    {
+        $cities      = new ArrayCollection($this->getDoctrine()->getRepository('ChapleanLocationBundle:City')->findAll());
+        $departments = new ArrayCollection($this->getDoctrine()->getRepository('ChapleanLocationBundle:Department')->findAll());
+        $regions     = new ArrayCollection($this->getDoctrine()->getRepository('ChapleanLocationBundle:Region')->findAll());
+
+        $location = ucwords(str_replace('_', ' ', str_replace(' ', '-', ucwords(str_replace('-', ' ', str_replace(' ', '_', $location))))));
+
+        $criteria = Criteria::create();
+
+        $citiesMatch = $cities->matching($criteria->where(Criteria::expr()->contains('name', $location))->orWhere(Criteria::expr()->contains('zipcode', $location)));
+        $departmentsMatch = $departments->matching($criteria->where(Criteria::expr()->contains('name', $location)));
+        $regionsMatch = $regions->matching($criteria->where(Criteria::expr()->contains('name', $location)));
+
+        $view = $this->view(array('results' => array_merge($citiesMatch->toArray(), $departmentsMatch->toArray(), $regionsMatch->toArray())));
+        $view->setSerializationContext(SerializationContext::create()->setGroups(array(
+            'location_id', 'location_name', 'city_zipcode'
+        )));
+
+        return $this->handleView($view);
+    }
+}
